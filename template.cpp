89e7ed1271cebc93c138069392f87632bde5c4ed
Ronnakorn Hompoa <36933333+kasettakorn@users.noreply.github.com>
2021-10-29 11:05:20 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-void solve(tc)
+void solve(int tc)
 {
+    int ans;
     cout << "Case #" << tc << ": " << ans;
 }
 int main()
 {
-    ios_base::sync_with_stdio(false); 
+    ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     int T;
     cin >> T;
