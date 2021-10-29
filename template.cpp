517a2640d9d64f2bc87d4f82bd9a6d78532ca5df
Ronnakorn Hompoa <36933333+kasettakorn@users.noreply.github.com>
2021-10-29 10:58:10 +0700

    Create template

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(tc)
+{
+    cout << "Case #" << tc << ": " << ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(0); cout.tie(0);
+    int T;
+    cin >> T;
+    for(int i=1; i<=T; i++) {
+        solve(i);
+    }
+    return 0;
+}
