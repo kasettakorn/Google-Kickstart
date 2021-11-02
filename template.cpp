504bc584f8d9fde187bbb5a422a8030a911fd29e
Ronnakorn Hompoa <36933333+kasettakorn@users.noreply.github.com>
2021-11-02 16:54:55 +0700

    Update define template

--- template.cpp
+++ template.cpp
@@ -1,9 +1,29 @@
 #include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i=a; i<b; i++)
+#define ROF(i,a,b) for(int i=b-1; i>=a; i--)
+#define SORT(a,n) sort(a, a+n)
+#define SORTR(a,n) sort(a, a+n, greater<int>())
+
 using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef pair<int, int> pii;
+typedef pair<int, char> pic;
+typedef pair<int, string> pis;
+typedef vector<pair<int,int>> vpii;
+
 void solve(int tc)
 {
     cout << "Case #" << tc << ": ";
+    
+
+
 }
+
 int main()
 {
     ios_base::sync_with_stdio(false);
